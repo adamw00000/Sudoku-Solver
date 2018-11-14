17de046707df531018ff1230f01506c82d7ecd9e
adamw00000 <adam.wawrzenczyk@gmail.com>
2018-11-14 16:48:44 +0100

    Moved to one file

--- Sudoku Solver/kernel.cu
+++ Sudoku Solver/kernel.cu
@@ -15,7 +15,151 @@
 #define SIZE 9
 
 typedef uint8_t byte;
+#include "SudokuCPU.h"
 
+class SudokuCPU
+{
+	bool resultSet = false;
+	byte board[SIZE][SIZE];
+	byte emptyFields[SIZE * SIZE];
+	uint32_t constraintStructures[SIZE];
+public:
+	byte result[SIZE][SIZE];
+
+	SudokuCPU(byte board[SIZE][SIZE], uint32_t constraintStructures[SIZE])
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			this->constraintStructures[i] = constraintStructures[i];
+			for (int j = 0; j < SIZE; j++)
+			{
+				this->board[i][j] = board[i][j];
+			}
+		}
+	}
+	
+	void GetEmptyFields()
+	{
+		int k = 0;
+		for (byte i = 0; i < SIZE; i++)
+		{
+			for (byte j = 0; j < SIZE; j++)
+			{
+				if (sudoku[i][j] == 0)
+				{
+					emptyFields[k] = i * SIZE + j;
+					k++;
+				}
+			}
+		}
+		for (; k < SIZE * SIZE; k++)
+		{
+			emptyFields[k] = -1;
+		}
+	}
+	
+	void SetResult()
+	{
+		resultSet = true;
+		for (int i = 0; i < SIZE; i++)
+		{
+			for (int j = 0; j < SIZE; j++)
+			{
+				result[i][j] = board[i][j];
+			}
+		}
+	}
+	
+	byte cell(byte i, byte j)
+	{
+		return (i / 3) * 3 + j / 3;
+	}
+	
+	void PrintSudoku(byte arr[SIZE][SIZE])
+	{
+		char s[200];
+		int k = 0;
+		for (int i = 0; i < SIZE; i++)
+		{
+			for (int j = 0; j < SIZE; j++)
+			{
+				s[k] = arr[i][j] + '0';
+				s[k + 1] = j == SIZE - 1 ? '\n' : ' ';
+				k += 2;
+			}
+		}
+		s[k] = '\n';
+		s[k + 1] = '\0';
+		printf("%s", s);
+	}
+	
+	void Solve()
+	{
+		Solve(0);
+	}
+	void Solve(int i)
+	{
+		if (i == SIZE * SIZE || emptyFields[i] == (byte)-1)
+		{
+			printf("CPU solution found!\n");
+			SetResult();
+			return;
+		}
+		if (resultSet)
+			return;
+	
+		int field = emptyFields[i];
+		byte row = field / SIZE;
+		byte col = field % SIZE;
+		
+		byte cellnr = cell(row, col);
+		
+		if (board[row][col] == 0)
+		{
+			for (byte number = 1; number <= SIZE; number++)
+			{
+				if (!IsNumberInConstraintStructure(number, constraintStructures[row], constraintStructures[col], constraintStructures[cellnr]))
+				{
+					board[row][col] = number;
+					AddNumberToConstraintStructure(number, constraintStructures[row], constraintStructures[col], constraintStructures[cellnr]);
+	
+					i++;
+					Solve(i);
+					i--;
+	
+					RemoveNumberFromRowOrColumn(number, constraintStructures[row], constraintStructures[col], constraintStructures[cellnr]);
+					board[row][col] = 0;
+				}
+			}
+		}
+	}
+	
+	bool IsNumberInConstraintStructure(byte number, const uint32_t& rowStructure, const uint32_t& colStructure, const uint32_t& cellStructure)
+	{
+		return (rowStructure & (1U << number) || colStructure & (1U << (SIZE + number)) || cellStructure & (1U << (SIZE + SIZE + number)));
+	}
+	
+	void AddNumberToConstraintStructure(byte number, uint32_t& rowStructure, uint32_t& colStructure, uint32_t& cellStructure)
+	{
+		if (number != 0)
+		{
+			rowStructure |= (1U << number);
+			colStructure |= (1U << (SIZE + number));
+			cellStructure |= (1U << (SIZE + SIZE + number));
+		}
+	}
+	
+	void RemoveNumberFromRowOrColumn(byte number, uint32_t& rowStructure, uint32_t& colStructure, uint32_t& cellStructure)
+	{
+		if (number != 0)
+		{
+			rowStructure &= ~(1U << number);
+			colStructure &= ~(1U << (SIZE + number));
+			cellStructure &= ~(1U << (SIZE + SIZE + number));
+		}
+	}
+	
+};
 struct Sudoku
 {
 	byte board[SIZE][SIZE];
